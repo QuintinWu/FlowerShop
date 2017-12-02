cce554c7c62044983e7583cd109dbedfe4b1769d
Kylin.this <kylinwu.thia@qq.com>
2017-12-03 00:26:24 +0800

    添加了首页控制器的几个视图页面，以及创建了用户模型User类和账户控制器Accounts

---
+++ Controllers/AccountsController.cs
@@ -0,0 +1,25 @@
+﻿using FlowerShopApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FlowerShopApp.Controllers
+{
+    public class AccountsController : Controller
+    {
+        // GET: Accounts
+        [HttpGet]
+        public ActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Register(User user)
+        {
+            return View();
+        }
+    }
+}
---
+++ Models/User.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FlowerShopApp.Models
+{
+    public class User
+    {
+        public String Username { get; set; }
+        public String Password { get; set; }
+        public String Email { get; set; }
+        public long Phone { get; set; }
+    }
+}
