2e098f9a414eec893c1973106119544ff2c06081
Kylin.this <kylinwu.thia@qq.com>
2017-12-02 20:38:23 +0800

    init project

---
+++ Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(FlowerShopApp.Startup))]
+namespace FlowerShopApp
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
