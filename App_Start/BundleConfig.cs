28bd85e42c7362fde6dc7ef87de8ae62f9b4577e
Kylin.this <kylinwu.thia@qq.com>
2017-12-02 21:36:19 +0800

    已经添加了样式表文件以及图片文件

--- App_Start/BundleConfig.cs
+++ App_Start/BundleConfig.cs
@@ -25,7 +25,10 @@ namespace FlowerShopApp
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                      "~/Content/site.css",
+                      "~/Content/flowershop.css",
+                    "~/Content/lightbox.css",
+                    "~/Content/style.css"));
         }
     }
 }
